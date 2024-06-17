94c7c75be5ddd53a8d37c2366e34b24e4946ef67
Jhonata Carvalho de Barros <jhonata.carvalho@tascomtecnologia.com.br>
2024-06-17 09:38:15 -0300

    [add] initial main route

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text>aaaaaaaaaa</Text>
+    </View>
+  );
+}
